bb6c52f63c2f6106c2e81bb0383087f57dc061ac
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-07-04 14:59:17 +0200

    links update + style

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
 
     <!-- Required meta tags -->
@@ -8,19 +9,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- CSS -->
-    <link rel="stylesheet" href="style.css">
-
-    <!-- Fontawesome kit -->
-    <script src="https://kit.fontawesome.com/2e455cc5f8.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/clock.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!-- JS script link -->
-    <script type="text/javascript" src="script.js"></script>
+    <script type="text/javascript" src="js/clock.js"></script>
 
     <title>O'clock</title>
 
 </head>
 
 <body>
+
+    <div class="icon-bar">
+        <a class="active" href="index.php"><i class="fa fa-clock-o"></i></a>
+        <a href="#"><i class="fa fa-search"></i></a>
+        <a href="#"><i class="fa fa-envelope"></i></a>
+        <a href="#"><i class="fa fa-globe"></i></a>
+        <a href="#"><i class="fa fa-trash"></i></a>
+    </div>
+
     <div class="clock" id="myClock"></div>
 </body>
+
 </html>
--- js/clock.js
+++ js/clock.js
@@ -8,26 +8,21 @@ function viewTime(){
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
-    let session = "AM";
 
     if(hours == 0){
         hours = 12;
-    }
-
-    if(hours > 12){
-        hours = hours - 12;
-        session = "PM";
-    }
+    }    
 
     hours = (hours < 10) ? "0" + hours : hours;
     minutes = (minutes < 10) ? "0" + minutes : minutes;
-    seconds = (seconds < 10) ? "0" + minutes : seconds;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-    let time = hours + ":" + minutes + ":" + seconds + " " + session;
+    let time = hours + ":" + minutes + ":" + seconds;
+    
     document.getElementById("myClock").innerText = time;
     console.log(time);
 
-    document.getElementById("myClock").textContent = time;
+    // document.getElementById("myClock").textContent = time;
 
     setTimeout(viewTime, 1000);
 
