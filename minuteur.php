4ca7f64a81071e8281b1206018bb892c6319b72f
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-07-05 09:26:08 +0200

    update timer page

--- minuteur.php
+++ minuteur.php
@@ -9,13 +9,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- CSS -->
-    <link rel="stylesheet" href="css/clock.css">
+    <link rel="stylesheet" href="css/minuteur.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!-- JS script link -->
-    <script type="text/javascript" src="js/clock.js"></script>
+    <script type="text/javascript" src="js/minuteur.js"></script>
 
-    <title>Horloge</title>
+    <title>Minuteur</title>
 
 </head>
 
@@ -23,8 +23,8 @@
     <header>
         <nav>
             <ul>
-                <li><a class="active" href="#">Horloge</a></li>
-                <li><a href="minuteur.php">Minuteur</a></li>
+                <li><a href="index.php">Horloge</a></li>
+                <li><a class="active" href="#">Minuteur</a></li>
                 <li><a href="reveil.php">Réveil</a></li>
                 <li><a href="chrono.php">Chronomètre</a></li>
             </ul>
--- reveil.php
+++ reveil.php
@@ -9,13 +9,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- CSS -->
-    <link rel="stylesheet" href="css/clock.css">
+    <link rel="stylesheet" href="css/reveil.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!-- JS script link -->
-    <script type="text/javascript" src="js/clock.js"></script>
+    <script type="text/javascript" src="js/reveil.js"></script>
 
-    <title>Horloge</title>
+    <title>Réveil</title>
 
 </head>
 
@@ -23,9 +23,9 @@
     <header>
         <nav>
             <ul>
-                <li><a class="active" href="#">Horloge</a></li>
+                <li><a href="index.php">Horloge</a></li>
                 <li><a href="minuteur.php">Minuteur</a></li>
-                <li><a href="reveil.php">Réveil</a></li>
+                <li><a class="active" href="#">Réveil</a></li>
                 <li><a href="chrono.php">Chronomètre</a></li>
             </ul>
         </nav>
